6338c41f43e1271931e71669fb6b2296bb387462
syeda sara <syedasaraahsan@gmail.com>
2023-05-07 22:58:42 +0600

    login

--- src/components/login.js
+++ src/components/login.js
@@ -10,11 +10,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useLoginMutation } from "../redux/commonApiCall";
 
 const Login = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
   const [user, loading, error] = useAuthState(auth);
-
+  
   const [login, { isLoading, error: loginError }] = useLoginMutation();
 
   useEffect(() => {
@@ -24,7 +24,18 @@ const Login = () => {
       router.push("/dashboard");
     }
   }, [user, loading]);
+  
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
+    try {
+      const { data } = await login({ email, password })
+      console.log(data)
+    } catch (error) {
+      alert(error)
+    }
+    
+  }
   return (
     <div className="flex justify-center items-center h-[100vh] bg-gray-400 ">
       <form className=" flex flex-col gap-4 border p-[40px]">
@@ -48,14 +59,14 @@ const Login = () => {
 
         <button
           className="p-2 hover:bg-gray-100 bg-blue-400 text-white hover:text-black "
-          onClick={(e) => signInWithEmailAndPassword(email, password)}
+          onClick={handleLogin}
         >
           Login
         </button>
 
         <button
           className="p-2 hover:bg-gray-100 bg-black text-white hover:text-black"
-          onClick={(e) => signInWithGoogle}
+          onClick={(e) => handleLogin}
         >
           Login with google
         </button>
